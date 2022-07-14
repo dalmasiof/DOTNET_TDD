4b06b0a83ae73d14abb695a08e225d9a260a4fea
Dalmasio Fernandes <dalmasiof@gmail.com>
2022-07-13 21:02:00 -0300

    collections Test

---
+++ Calculations/Calculations/Costumer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations
+{
+    public class Costumer
+    {
+        public string Name => "Dalmasio";
+        public int Age => 25;
+    }
+}
--- Calculations/CalculationsTest/FibonacciTests.cs
+++ Calculations/CalculationsTest/FibonacciTests.cs
@@ -20,5 +20,28 @@ namespace CalculationsTest
             //Assert
             Assert.All(result, n => Assert.NotEqual(0, n));
         }
+
+        [Fact]
+        public void Fibonacci_has13()
+        {
+            //Arrange
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            //Act
+            var result = fibonacci.Fibonacci;
+            //Assert
+            Assert.Contains(13,result);
+        }
+
+
+        [Fact]
+        public void Fibonacci_hasNo4()
+        {
+            //Arrange
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            //Act
+            var result = fibonacci.Fibonacci;
+            //Assert
+            Assert.DoesNotContain(4, result);
+        }
     }
 }
